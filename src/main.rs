f3719a0ad8c5ec511ee352cd4994818a252e7dcd
Camilo MATAJIRA <camilo.matajira@powens.com>
2026-02-05 13:12:04 +0100

    First test passes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,86 @@
+use regex::Regex;
+use serde_json::{Map, Result, Value};
 fn main() {
-    println!("Hello, world!");
+    untyped_example();
+}
+
+fn untyped_example() -> Result<()> {
+    // Some JSON input data as a &str. Maybe this comes from the user.
+    let data = r#"
+        {
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ]
+        }"#;
+
+    // Parse the string of data into serde_json::Value.
+    let v: Value = serde_json::from_str(data)?;
+
+    // Access parts of the data by indexing with square brackets.
+    println!("Please call {} at the number {}", v["name"], v["phones"][0]);
+    print(&v);
+
+    Ok(())
+}
+fn print(v: &Value) {
+    match v {
+        Value::Object(v) => {
+            println! {"Object with {} keys", v.len()};
+            for (k, v) in v {
+                println! {"Key: {}", k};
+                print(v);
+            }
+            // print(&v);
+        }
+        Value::String(v) => {
+            println! {"String with length {}", v};
+        }
+        Value::Array(v) => {
+            println! {"Array of length {}", v.len()};
+        }
+        Value::Null => {}
+        Value::Bool(v) => {
+            println! {"Boolean with value {}", v};
+        }
+        Value::Number(v) => {
+            println! {"Number with value {}", v};
+        }
+    };
+}
+
+fn key_substitute(v: Value, old_regexp: String, new_regexp: String) -> Value {
+    let re = Regex::new(&old_regexp).unwrap();
+    // print(&v);
+    let v = match v {
+        Value::Object(old_map) => {
+            let mut new_map: Map<String, Value> = Map::new();
+            for (k, v) in old_map {
+                let new_key = re.replace(&k, &new_regexp).into_owned();
+                new_map.insert(new_key, v);
+            }
+            Value::Object(new_map)
+        }
+        Value::String(v) => Value::String(v),
+        Value::Array(v) => Value::Array(v),
+        Value::Null => Value::Null,
+        Value::Bool(v) => Value::Bool(v),
+        Value::Number(v) => Value::Number(v),
+    };
+    print(&v);
+    return v;
+}
+
+mod tests {
+    use super::*;
+    #[test]
+    fn test_key_substitute_1() {
+        let some_json = r#"
+        {"sha": "0eb3da11ed489189963045a3d4eb21ba343736cb", "node_id": "C_kwDOAE3WVdoAKDBlYjNkYTExZWQ0ODkxODk5NjMwNDVhM2Q0ZWIyMWJhMzQzNzM2Y2I"}"#;
+        let mut v: Value = serde_json::from_str(some_json).unwrap();
+        v = key_substitute(v, String::from("sha"), String::from("new_sha"));
+        assert_eq!(v["new_sha"], "0eb3da11ed489189963045a3d4eb21ba343736cb");
+    }
 }
